f9455ef840c90159264771f708f2ac35f90086db
Andrey M. <mashenko.andr@gmail.com>
2024-01-31 14:51:01 +0100

    Added unit tests to ensure that validation is correct

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# api-crypto-cicd
+# Individuell examination CI/CD
+
+AES encryption and decryption WEB API
+
+01-29 Created basic Encrypting program logic
+
+For encryption use endpoint:
+
+http://encryptionapi-env.eba-mp8gwi3q.eu-north-1.elasticbeanstalk.com/encryption/encrypt?key=6TzgI/g7Qlg1EZzzYIZR4g==&plaintext=YOUR_TEXT
+
+For decryption:
+
+http://encryptionapi-env.eba-mp8gwi3q.eu-north-1.elasticbeanstalk.com/encryption/decrypt?key=6TzgI/g7Qlg1EZzzYIZR4g==/YOUR_ENCRYPTED_TEXT
+
+"6TzgI/g7Qlg1EZzzYIZR4g==" is a key
+
+To-do:
+
+Web connected to API on elastic beanstalk and key generation with JS
+
---
+++ UnitTests/ServiceTests.cs
@@ -0,0 +1,78 @@
+using System;
+using Xunit;
+using AESWebAPI;
+
+namespace UnitTests
+{
+    public class ServiceTests
+    {
+        [Fact]
+        public void Encrypt_GivenPlainTextAndKey_ReturnsEncryptedText()
+        {
+            // Arrange
+            var encryptionService = new AESEncryptionService();
+            string plainText = "Hello, David!";
+            string key = "giUvo4vuNheChPfP7hNWPQ==";
+
+            // Act
+            string encryptedText = encryptionService.Encrypt(plainText, key);
+
+            // Assert
+            Assert.NotNull(encryptedText);
+        }
+
+        [Fact]
+        public void Decrypt_GivenEncryptedTextAndKey_ReturnsDecryptedText()
+        {
+            // Arrange
+            var encryptionService = new AESEncryptionService();
+            string encryptedText = "giUvo4vuNheChPfP7hNWPQ==";
+            string key = "1/y3hSibEgZwpf04";
+
+            // Act
+            string decryptedText = encryptionService.Decrypt(encryptedText, key);
+
+            // Assert
+            Assert.NotNull(decryptedText);
+        }
+
+        [Fact]
+        public void Decrypt_GivenIncorrectKey_ThrowsArgumentException()
+        {
+            // Arrange
+            var encryptionService = new AESEncryptionService();
+            string encryptedText = "giUvo4vuNheChPfP7hNWPQ==";
+            string key = "IncorrectKey";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => encryptionService.Decrypt(encryptedText, key));
+        }
+        
+        [Fact]
+        public void Decrypt_GivenInvalidEncryptedText_ReturnsNull()
+        {
+            // Arrange
+            var encryptionService = new AESEncryptionService();
+            string encryptedText = "InvalidEncryptedText";
+            string key = "giUvo4vuNheChPfP7hNWPQ==";
+
+            // Act
+            string decryptedText = encryptionService.Decrypt(encryptedText, key);
+
+            // Assert
+            Assert.Null(decryptedText);
+        }
+        
+        [Fact]
+        public void Encrypt_GivenNullPlainText_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var encryptionService = new AESEncryptionService();
+            string plainText = null;
+            string key = "giUvo4vuNheChPfP7hNWPQ==";
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => encryptionService.Encrypt(plainText, key));
+        }
+    }
+}
